bfe5ebefbb759e77784327e3240199233d458eec
Pablo Correa Gómez <pablo.correa.gomez@combine.se>
2023-04-13 16:18:43 +0200

    [dataset] + DatasetList.iterate_chain
    This method allows to iterate the complete chain of every dataset
    in a list of datasets. There is no check that chains do not contain
    duplicated datasets, but the user is responsible for that. The main
    usecase in this situation is when data from multiple machines is
    imported into the accelerator in their own urd lists. Each of those
    machines can have many log files, thus creating a chain. When the
    need exists to iterate over the datasets for multiple machines at a
    time, having an iterate_chain is very convenient. It allows to
    have a single method with "datasets = (['source'],)" that can analyze
    the whole data from all the given machines.

--- accelerator/dataset.py
+++ accelerator/dataset.py
@@ -1722,8 +1722,20 @@ class DatasetList(_ListTypePreserver):
 		return any(ds.columns[column].none_support for ds in self if column in ds.columns)
 
 	def iterate(self, sliceno, columns=None, range=None, sloppy_range=False, hashlabel=None, pre_callback=None, post_callback=None, filters=None, translators=None, status_reporting=True, rehash=False, slice=None, copy_mode=False):
-		"""Iterate the datasets in this chain. See Dataset.iterate_list for usage"""
+		"""Iterate the datasets in this list. See Dataset.iterate_list for usage"""
 		return Dataset.iterate_list(sliceno, columns, self, range=range, sloppy_range=sloppy_range, hashlabel=hashlabel, pre_callback=pre_callback, post_callback=post_callback, filters=filters, translators=translators, status_reporting=status_reporting, rehash=rehash, slice=slice, copy_mode=copy_mode)
+
+	def iterate_chain(self, sliceno, columns=None, length=-1, range=None, sloppy_range=False, reverse=False, hashlabel=None, stop_ds=None, pre_callback=None, post_callback=None, filters=None, translators=None, status_reporting=True, rehash=False, slice=None, copy_mode=False):
+		"""
+		Iterate the datasets in this list, and all its chained ones.
+		See Dataset.iterate_chain and Dataset.iterate_list for details.
+		"""
+		if stop_ds and len(stop_ds) != len(self):
+			raise DatasetUsageError('len stop_ds must be equal to list')
+		chain = DatasetChain()
+		for ix, ds in enumerate(self):
+			chain.extend(ds.chain(length, reverse, stop_ds[ix] if stop_ds else None))
+		return Dataset.iterate_list(sliceno, columns, chain, range=range, sloppy_range=sloppy_range, hashlabel=hashlabel, pre_callback=pre_callback, post_callback=post_callback, filters=filters, translators=translators, status_reporting=status_reporting, rehash=rehash, slice=slice, copy_mode=copy_mode)
 
 	def range(self, colname, start=None, stop=None):
 		"""Filter out only datasets where colname has values in range(start, stop)"""
