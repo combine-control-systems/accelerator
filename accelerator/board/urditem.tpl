combine-control-systems/accelerator

commit 91cecfc9aff70945cbb2a6bb51a0575778bc6fc2

[board] fade out urd timestamp separators a bit
and add a tiny bit of padding around them. For better readability.
And do the same for the path separators too.

accelerator/board/urditem.tpl
@@ -1,9 +1,10 @@
 {{ ! template('head', title=key) }}
 
-	<h1>{{ key }}</h1>
+	<h1>{{ ! urd_ts_html(key) }}</h1>
 	<table class="urd-table">
 		% saved_caption = entry.caption
-		% for thing in ('timestamp', 'user', 'build', 'caption',):
+		<tr><td>timestamp</td><td id="urd-timestamp">{{ ! urd_ts_html(entry.pop('timestamp')) }}</td></tr>
+		% for thing in ('user', 'build', 'caption',):
 			<tr><td>{{ thing }}</td><td id="urd-{{ thing }}">{{ entry.pop(thing) }}</td></tr>
 		% end
 		% if entry.build_job:
@@ -21,8 +22,8 @@
 	<table class="urd-table">
 		<tr><td>deps</td><td>
 			% for dep, depentry in sorted(entry.deps.items()):
-				<a href="/urd/{{ url_quote(dep) }}/{{ depentry.timestamp }}">
-					{{ dep }}/{{ depentry.timestamp }}
+				<a href="/urd/{{ url_quote(dep) }}/{{ ! urd_ts_html(depentry.timestamp) }}">
+					{{ dep }}/{{ ! urd_ts_html(depentry.timestamp) }}
 				</a>
 				<ol>
 					% for method, job in depentry.joblist:

accelerator/board.py
@@ -163,6 +163,14 @@ def js_quote(obj):
 	return json.dumps(obj).replace('<', '\\074')
 
 
+def urd_ts_html(ts):
+	ts = [bottle.html_escape(part) for part in ts.split('/')]
+	if len(ts) != 2: # Full path or just ts, but not when "user/list"
+		ts[-1] = ts[-1].replace('+', '<span class="urd-ts-sep">+</span>')
+		ts[-1] = ts[-1].replace('T', '<span class="urd-ts-sep">T</span>')
+	return '<span class="urd-ts-sep">/</span>'.join(ts)
+
+
 def template(tpl_name, **kw):
 	return bottle.template(
 		tpl_name,
@@ -173,6 +181,7 @@ def template(tpl_name, **kw):
 		name2hashed=name2hashed,
 		template=template,
 		url_quote=url_quote,
+		urd_ts_html=urd_ts_html,
 		**kw
 	)
 

accelerator/board/style.css
@@ -485,6 +485,11 @@ ul.urdlist > li > a:first-child {
 	margin-right: 1em;
 }
 
+.urd-ts-sep {
+	opacity: 0.6;
+	padding: 0 0.05em;
+}
+
 table.job-table {
 	border-spacing: 0 2px;
 	clear: both;

accelerator/board/urd.tpl
@@ -4,7 +4,7 @@
 	<ul class="urdlist">
 	% for thing in lists:
 		<li>
-			<a href="/urd/{{ url_quote(thing) }}">{{ thing }}</a>
+			<a href="/urd/{{ url_quote(thing) }}">{{ ! urd_ts_html(thing) }}</a>
 			<a href="/urd/{{ url_quote(thing) }}/first">first</a>
 			<a href="/urd/{{ url_quote(thing) }}/latest">latest</a>
 		</li>

accelerator/board/urdlist.tpl
@@ -1,9 +1,9 @@
 {{ ! template('head', title=key) }}
 
-	<h1>{{ key }}</h1>
+	<h1>{{ ! urd_ts_html(key) }}</h1>
 	<ul class="urdlist">
 	% for ts, caption in timestamps:
-		<li><a href="/urd/{{ url_quote(key) }}/{{ url_quote(ts) }}">{{ ts }}</a> <span class="urd-caption">{{ caption[:70] + '....' if len(caption) > 80 else caption }}</span></li>
+		<li><a href="/urd/{{ url_quote(key) }}/{{ url_quote(ts) }}">{{ ! urd_ts_html(ts) }}</a> <span class="urd-caption">{{ caption[:70] + '....' if len(caption) > 80 else caption }}</span></li>
 	% end
 	</ul>
 <script language="javascript">

commit 252468b17f1a94fd93e3c520d2338d748f31baa8

[board] fix broken dep URLs
broken in 91cecfc9 [board] fade out urd timestamp separators a bit.

accelerator/board/urditem.tpl
@@ -22,7 +22,7 @@
 	<table class="urd-table">
 		<tr><td>deps</td><td>
 			% for dep, depentry in sorted(entry.deps.items()):
-				<a href="/urd/{{ url_quote(dep) }}/{{ ! urd_ts_html(depentry.timestamp) }}">
+				<a href="/urd/{{ url_quote(dep) }}/{{ depentry.timestamp }}">
 					{{ dep }}/{{ ! urd_ts_html(depentry.timestamp) }}
 				</a>
 				<ol>