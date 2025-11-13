ddd9b6342e5182d36c2f1a8afb7ce6a79a8c83ca
Carl Drougge <bearded@longhaired.org>
2025-11-13 21:06:56 +0100

    + dataset_join_columns

---
+++ accelerator/standard_methods/a_dataset_join_columns.py
@@ -0,0 +1,94 @@
+############################################################################
+#                                                                          #
+# Copyright (c) 2025 Carl Drougge                                          #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License");          #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#  http://www.apache.org/licenses/LICENSE-2.0                              #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+#                                                                          #
+############################################################################
+
+description = r'''
+Joins two or more columns with an optional separator.
+
+Supports string-like types and also date + time joining into a datetime column.
+'''
+
+
+from datetime import datetime
+from itertools import starmap
+from functools import partial
+
+
+datasets = ('source', 'previous',)
+
+options = dict(
+	columns=[str],
+	colname='joined column',
+	discard_source_columns=False,
+	separator='',
+)
+
+
+def prepare(job):
+	if len(options.columns) < 2:
+		raise Exception("Specify at least two columns")
+	chain = datasets.source.chain(stop_ds={datasets.previous: 'source'})
+	allowed = [
+		{'ascii'},
+		{'bytes'},
+		{'unicode'},
+	]
+	if len(options.columns) == 2:
+		allowed.append({'date', 'time'})
+	for ds in chain:
+		types = set()
+		for col in options.columns:
+			if col not in ds.columns:
+				raise Exception(f"Column {col !r} doesn't exist in {ds.quoted}")
+			types.add(ds.columns[col].type)
+		if types not in allowed:
+			if types == {'date', 'time'}:
+				raise Exception(f"Can't join {len(options.columns)} colums of date and time (in {ds.quoted})")
+			raise Exception(f"Unjoinable column types {types} in {ds.quoted}")
+	names = list(map(str, range(len(chain))))
+	names[-1] = 'default'
+	previous = datasets.previous
+	writers = []
+	columns = dict.fromkeys(options.columns) if options.discard_source_columns else {}
+	for ds, name in zip(chain, names):
+		desttype = ds.columns[options.columns[0]].type
+		if desttype in ('date', 'time'):
+			desttype = 'datetime'
+		columns[options.colname] = desttype
+		dw = job.datasetwriter(name=name, previous=previous, parent=ds, columns=columns)
+		writers.append(dw)
+		previous = dw
+	return writers
+
+
+def analysis(sliceno, prepare_res):
+	joiners = {
+		'ascii'  : partial(map, options.separator.join),
+		'bytes'  : partial(map, options.separator.encode('utf-8').join),
+		'date'   : partial(starmap, datetime.combine),
+		'unicode': partial(map, options.separator.join),
+	}
+	for writer in prepare_res:
+		ds = writer.parent
+		write = writer.write
+		columns = options.columns
+		typ = ds.columns[columns[0]].type
+		if typ == 'time':
+			typ = 'date'
+			columns = [columns[1], columns[0]]
+		for value in joiners[typ](ds.iterate(sliceno, columns)):
+			write(value)
